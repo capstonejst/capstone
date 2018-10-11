16873aba8e562641f5a0af56df64fe2eda9399ba
tricialobo <40130654+tricialobo@users.noreply.github.com>
2018-10-11 12:28:44 -0400

    Delete factory.js

--- ethereum/factory.js
+++
@@ -1,8 +0,0 @@
-const web3 = require('./web3')
-const BlockFactory = require('./build/BlockFactory.json')
-
-const instance = new web3.eth.Contract(
-  JSON.parse(BlockFactory.interface),
-  '0xD2C91E0F7932ed59cEf629bCB15b6bC5ED51C14b'
-)
-module.exports = instance
